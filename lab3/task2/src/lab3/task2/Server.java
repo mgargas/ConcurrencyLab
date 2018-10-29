8c5620e08672917495108c3b00293f57c0d10d13
Marek Gargas <250497@wp.pl>
2018-10-29 20:30:48 +0100

    clean code and delete useless locks

--- lab3/task2/src/lab3/task2/Server.java
+++ lab3/task2/src/lab3/task2/Server.java
@@ -9,25 +9,20 @@ public class Server {
     HashMap<Integer, Guest> guests = new HashMap<>();
     private Boolean[] presentGuests;
     private Boolean[] pairHavingTable;
-    final Lock lock = new ReentrantLock();
-    final Condition table  = lock.newCondition();
-    final Condition currentlyWaiting = lock.newCondition();
-    HashMap<Integer, Lock> pairLocks = new HashMap<>();
-    HashMap<Integer, Condition> pairMissingSomeone = new HashMap<>();
+    private final Lock lock = new ReentrantLock();
+    private final Condition table  = lock.newCondition();
+    private HashMap<Integer, Condition> pairMissingTable = new HashMap<>();
+    private HashMap<Integer, Condition> pairMissingSomeone = new HashMap<>();
     private boolean isReserved = false;
-    private int guestsAmount;
 
     public Server(HashMap<Integer, Guest> guests, int guestsAmount){
         this.guests = guests;
-        this.guestsAmount = guestsAmount;
         this.presentGuests = new Boolean[guestsAmount];
         this.pairHavingTable = new Boolean[guestsAmount/2];
         for(int i=0; i<guestsAmount; i++) this.presentGuests[i] = false;
         for(int i=0; i<guestsAmount/2; i++){
-            Lock pair_lock = new ReentrantLock();
-            Condition pair_condition = pair_lock.newCondition();
-            pairLocks.put(i, pair_lock);
-            pairMissingSomeone.put(i, pair_condition);
+            pairMissingSomeone.put(i, lock.newCondition());
+            pairMissingTable.put(i, lock.newCondition());
             pairHavingTable[i] = false;
         }
     }
@@ -40,34 +35,26 @@ public class Server {
         Integer pairID = guests.get(guestID).getPairID();
         if(!presentGuests[anotherGuestID]){
             System.out.println("Guest#" + guestID + " is waiting for Guest#" + anotherGuestID);
-            Lock pairLock = pairLocks.get(pairID);
-            Condition pairCondition = pairMissingSomeone.get(pairID);
-            lock.unlock();
-            pairLock.lock();
-            pairCondition.await();
-            lock.lock();
+            pairMissingSomeone.get(pairID).await();
             System.out.println("Guest#" + guestID + " has just found out that Guest#" + anotherGuestID + " has come too");
-            System.out.println("Guest#" + guestID + " is waiting for the table");
+            System.out.println("Guest#" + guestID + " is checking if the table is available.");
             while (isReserved){
+                System.out.println("Guest#" + guestID + " is waiting for the table.");
                 table.await();
             }
             isReserved = true;
             pairHavingTable[pairID] = true;
             System.out.println("Guest#" + guestID + " has reserved the table");
-            currentlyWaiting.signal();
-            lock.unlock();
+            pairMissingTable.get(pairID).signal();
 
         }else {
-            Lock pairLock = pairLocks.get(pairID);
-            pairLock.lock();
             pairMissingSomeone.get(pairID).signal();
-            pairLock.unlock();
             while(!pairHavingTable[pairID]){
-                currentlyWaiting.await();
+                pairMissingTable.get(pairID).await();
             }
             System.out.println("Guest#" + guestID + " has just found out that Guest#" + anotherGuestID + " has reserved the table.");
-            lock.unlock();
         }
+        lock.unlock();
 
     }
 
@@ -75,18 +62,16 @@ public class Server {
         lock.lock();
         System.out.println("Guest#" + guestID + " is leaving the table");
         Integer anotherGuestID = guests.get(guestID).getAnotherGuestID();
-        Integer pairID = guests.get(guestID).getPairID();
         if(!presentGuests[anotherGuestID]){
             isReserved = false;
             System.out.println("Guest#" + guestID + " is releasing the table");
             table.signal();
             presentGuests[guestID] = false;
             System.out.println("Guest#" + guestID + " is leaving the restaurant");
-            lock.unlock();
         }else{
             presentGuests[guestID] = false;
             System.out.println("Guest#" + guestID + " is leaving the restaurant and Guest#" + anotherGuestID);
-            lock.unlock();
         }
+        lock.unlock();
     }
 }
