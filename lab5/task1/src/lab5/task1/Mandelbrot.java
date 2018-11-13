2037c8091cdcf9e3c8f6b306899bd2eb617e00d6
Marek Gargas <250497@wp.pl>
2018-11-13 23:44:15 +0100

    add lab5

---
+++ lab5/task1/src/lab5/task1/Mandelbrot.java
@@ -0,0 +1,105 @@
+package lab5.task1;
+import com.opencsv.CSVWriter;
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+import javax.swing.JFrame;
+public class Mandelbrot extends JFrame{
+    private final int MAX_ITER = 570;
+    private final double ZOOM = 150;
+    private BufferedImage I;
+    private double zx, zy, cX, cY, tmp;
+
+    public Mandelbrot() {
+        super("Mandelbrot Set");
+        setBounds(100, 100, 800, 600);
+        setResizable(false);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        I = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_RGB);
+        for (int y = 0; y < getHeight(); y++) {
+            for (int x = 0; x < getWidth(); x++) {
+                zx = zy = 0;
+                cX = (x - 400) / ZOOM;
+                cY = (y - 300) / ZOOM;
+                int iter = MAX_ITER;
+                while (zx * zx + zy * zy < 4 && iter > 0) {
+                    tmp = zx * zx - zy * zy + cX;
+                    zy = 2.0 * zx * zy + cY;
+                    zx = tmp;
+                    iter--;
+                }
+                I.setRGB(x, y, iter | (iter << 8));
+            }
+        }
+    }
+
+    public int getMAX_ITER() {
+        return MAX_ITER;
+    }
+
+    public double getZOOM() {
+        return ZOOM;
+    }
+
+    public BufferedImage getI() {
+        return I;
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        g.drawImage(I, 0, 0, this);
+    }
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException, IOException {
+
+        File file = new File("./measurements.csv");
+        FileWriter outputFile = new FileWriter(file);
+        CSVWriter writer = new CSVWriter(outputFile);
+        String[] header = { "threadsAmount","tasksAmount", "time" };
+        writer.writeNext(header);
+        Integer[] threadsAmount = {1, 4, 8};
+        Integer[] tasksAmount = {1, 4, 8, 10, 40, 80, 600*800};
+        Mandelbrot mandelbrot = new Mandelbrot();
+        mandelbrot.setVisible(true);
+        for(Integer  threads: threadsAmount){
+            for(Integer tasks: tasksAmount){
+                System.out.println(threads + " " + tasks);
+                ExecutorService executorService = Executors.newFixedThreadPool(threads);
+                List<Future> futureList = new ArrayList<>();
+                int dx = mandelbrot.getWidth()/tasks;
+                int dy = mandelbrot.getHeight()/tasks;
+                for(int i=0; i<10; i++) {
+                    int x = 0;
+                    int y = 0;
+                    for (int j = 0; j < tasks; j++) {
+                        Future future = executorService.
+                                submit(new MandelbrotDrawer(mandelbrot, x, x + dx, y, y + dy));
+                        x += dx;
+                        y += dy;
+                        futureList.add(future);
+                    }
+                    List<Long> tasksTimeList = new ArrayList<>();
+                    for (Future future : futureList) {
+                        tasksTimeList.add((Long) future.get());
+                    }
+                    Long time = tasksTimeList.stream().mapToLong(a -> a).sum();
+                    String[] csvRecord = {String.valueOf(threads), String.valueOf(tasks), String.valueOf(time)};
+                    writer.writeNext(csvRecord);
+                }
+            }
+
+        }
+        System.out.println("I am finishing!");
+        writer.close();
+
+        //executorService.submit(new MandelbrotDrawer(mandelbrot, 0, mandelbrot.getWidth(), 0, mandelbrot.getHeight()));
+    }
+}
---
+++ lab5/task1/src/lab5/task1/MandelbrotDrawer.java
@@ -0,0 +1,42 @@
+package lab5.task1;
+
+import java.util.concurrent.Callable;
+
+public class MandelbrotDrawer implements Callable<Long>{
+    private Mandelbrot mandelbrot;
+    private int x1;
+    private int x2;
+    private int y1;
+    private int y2;
+
+    public MandelbrotDrawer(Mandelbrot mandelbrot, int x1, int x2, int y1, int y2){
+        this.mandelbrot = mandelbrot;
+        this.x1 = x1;
+        this.x2 = x2;
+        this.y1 = y1;
+        this.y2 = y2;
+    }
+
+    @Override
+    public Long call() {
+        double zx, zy, cX, cY, tmp;
+        Long startTime = System.nanoTime();
+        for (int y1 = 0; y1 < y2; y1++) {
+            for (int x1 = 0; x1 < x2; x1++) {
+                zx = zy = 0;
+                cX = (x1 - 400) / mandelbrot.getZOOM();
+                cY = (y1 - 300) / mandelbrot.getZOOM();
+                int iter = mandelbrot.getMAX_ITER();
+                while (zx * zx + zy * zy < 4 && iter > 0) {
+                    tmp = zx * zx - zy * zy + cX;
+                    zy = 2.0 * zx * zy + cY;
+                    zx = tmp;
+                    iter--;
+                }
+                mandelbrot.getI().setRGB(x1, y1, iter | (iter << 8));
+            }
+        }
+        Long endTime = System.nanoTime();
+        return endTime - startTime;
+    }
+}
